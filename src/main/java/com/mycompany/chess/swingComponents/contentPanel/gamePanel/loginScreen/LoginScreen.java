405c322c0c5370f6879a1807b746f4a40c38ab30
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-09-22 20:11:39 -0300

    Added popup to show login error

--- src/main/java/com/mycompany/chess/swingComponents/contentPanel/gamePanel/loginScreen/LoginScreen.java
+++ src/main/java/com/mycompany/chess/swingComponents/contentPanel/gamePanel/loginScreen/LoginScreen.java
@@ -7,6 +7,7 @@ package com.mycompany.chess.swingComponents.contentPanel.gamePanel.loginScreen;
 import com.mycompany.chess.swingComponents.contentPanel.gamePanel.GamePanel;
 import entitites.NullPlayerException;
 import java.awt.CardLayout;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -31,7 +32,6 @@ public class LoginScreen extends javax.swing.JPanel {
     private void initComponents() {
 
         loginButton = new javax.swing.JButton();
-        errorLabel = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(51, 51, 51));
         setMinimumSize(new java.awt.Dimension(1009, 688));
@@ -43,21 +43,13 @@ public class LoginScreen extends javax.swing.JPanel {
             }
         });
 
-        errorLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        errorLabel.setForeground(new java.awt.Color(255, 0, 51));
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(462, 462, 462)
-                        .addComponent(loginButton))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(113, 113, 113)
-                        .addComponent(errorLabel)))
+                .addGap(462, 462, 462)
+                .addComponent(loginButton)
                 .addContainerGap(475, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -65,15 +57,14 @@ public class LoginScreen extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(326, 326, 326)
                 .addComponent(loginButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 289, Short.MAX_VALUE)
-                .addComponent(errorLabel)
-                .addGap(50, 50, 50))
+                .addContainerGap(339, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     public void showError(NullPlayerException e){
-        errorLabel.setText(e.getMessage());
+        JOptionPane.showMessageDialog(this, e.getMessage());
     }
+    
     private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginButtonActionPerformed
         // TODO add your handling code here:
         GamePanel gamePanel = (GamePanel) this.getParent();
@@ -82,7 +73,6 @@ public class LoginScreen extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel errorLabel;
     private javax.swing.JButton loginButton;
     // End of variables declaration//GEN-END:variables
 }
