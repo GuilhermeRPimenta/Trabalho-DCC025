8fc2a637d6df3965a2af76a708cb655357725e7e
Victor Wingert de Almeida <victor.wingert@estudante.ufjf.br>
2024-09-29 23:42:39 -0300

    Add Nome e Matricula

--- src/main/java/com/mycompany/chess/Chess.java
+++ src/main/java/com/mycompany/chess/Chess.java
@@ -1,3 +1,8 @@
+//Victor Wingert de Almeida         - 202335036
+//Guilherme Roldão dos Reis Pimenta - 202365085A
+//Bruno Nascimento Rodrigues        - 202065114A
+//Pablo Novaes Costa                - 202335031
+
 package com.mycompany.chess;
 
 public class Chess {
--- src/main/java/entitites/NullPlayerException.java
+++ src/main/java/entitites/NullPlayerException.java
@@ -1,3 +1,8 @@
+//Victor Wingert de Almeida         - 202335036
+//Guilherme Roldão dos Reis Pimenta - 202365085A
+//Bruno Nascimento Rodrigues        - 202065114A
+//Pablo Novaes Costa                - 202335031
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
